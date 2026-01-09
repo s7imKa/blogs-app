49a9a980e4b06c8e48c082d7e57f735b19688589
s7imKa <maksikmaksik2005@gmail.com>
2026-01-09 19:44:14 +0200

    validation methods for forms have been created

---
+++ src/utils/validators.ts
@@ -0,0 +1,8 @@
+export const validateEmail = (email: string) => {
+    const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    return re.test(String(email).toLowerCase())
+}
+
+export const validatePassword = (password: string) => {
+    return password.length >= 6
+}
